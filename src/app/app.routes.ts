fba590a1ddc440d9e4619b23fb8fceef5064a6cb
sfernandezso <sacha.fernandez-soltane@etud.univ-rennes1.fr>
2024-04-10 00:21:07 +0200

    Adding navbar + login algo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { FormSignInComponent } from './form-sign-in/form-sign-in.component';
+import { LogoutComponent } from './logout/logout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'login', component: FormSignInComponent}
+  { path: 'login', component: FormSignInComponent},
+  { path: 'logout', component: LogoutComponent},
+  { path: 'home', component: HomeComponent}
 ];
--- src/app/form-sign-in/form-sign-in.component.ts
+++ src/app/form-sign-in/form-sign-in.component.ts
@@ -3,6 +3,7 @@ import {MatCardModule} from '@angular/material/card';
 import { AuthService } from '../auth.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-sign-in',
@@ -15,20 +16,37 @@ export class FormSignInComponent {
   email: string = "";
   password: string = "" ;
 
-  constructor(private http: HttpClient,private authService: AuthService) { 
-    console.log('on arrive ici');
+  constructor(private http: HttpClient,private authService: AuthService, private router: Router) { 
+  }
+
+  ngOnInit(): void {
+    // Vérifier si l'utilisateur est déjà connecté lors de l'initialisation du composant
+    if (this.authService.getToken()) {
+      // Rediriger l'utilisateur vers la page principale
+      this.router.navigateByUrl('/');
+    }
   }
 
   onSubmit() {
-    console.log(this.email);
-    console.log(this.password);
     this.authService.login(this.email, this.password).subscribe(
-      response => {
-        // Gérer la réponse de l'API (par exemple, stocker le token d'authentification)
+      (response) => {
+        // Vérifier si la réponse contient un message SUCCESS
+        if (response && response.message === "SUCCESS") {
+          // Gérer la connexion réussie ici
+          console.log("Connexion réussie");
+          this.router.navigateByUrl('/home');
+          // Rediriger l'utilisateur, afficher un message de succès, etc.
+        } else {
+          console.error("Réponse de connexion invalide :", response);
+          // Afficher un message d'erreur générique ou effectuer une autre action en cas de réponse invalide
+        }
       },
-      error => {
-        // Gérer les erreurs de connexion
+      (error) => {
+        console.error("Erreur lors de la connexion :", error);
+        // Gérer les erreurs de la requête HTTP ici
+        // Afficher un message d'erreur à l'utilisateur, nettoyer les champs de formulaire, etc.
       }
     );
   }
+  
 }
